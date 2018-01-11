bff82c3107a22aa3b58c61ce8aeac8a29436409a
Dmitri Stefanisin <email.ds@protonmail.com>
2018-01-11 14:21:09 -0600

    add better populer method, but need to be fixed

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,10 @@ get '/' do
   if params.empty?
     all_posts = Post.all.order('created_at': :desc)
   else
-    all_posts = Post.all
+    likes_array = Post.all.map(&:likes).map(&:count)
+    average_like = likes_array.reduce(:+) / likes_array.size
+    all_posts = Post.all.select { |post| post.like_count > average_like }
+    # all_posts = Post.popular
   end
   @posts = all_posts
   erb :index
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   has_many :favorite_posts
-  has_many :likes, as: :likable 
+  has_many :likes, as: :likable
+
+  # need to fix it
+  # scope :popular, lambda {
+  #   select("post.id,post.description,post.pic_link, count(likes.id) AS likes_count").
+  #   joins(:likes).
+  #   group("posts.id").
+  #   order("likes_count DESC")
+  # }
+
+  def like_count
+    self.likes.count
+  end
 end
