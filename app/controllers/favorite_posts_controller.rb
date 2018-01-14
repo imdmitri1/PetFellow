eee6108f3b2d186529389c45a5ae84f1009cb456
Dmitri Stefanisin <email.ds@protonmail.com>
2018-01-14 08:18:58 -0600

    add post route for favorite/unfavorite + buttons in views, need to make buttons changing

--- app/controllers/favorite_posts_controller.rb
+++ app/controllers/favorite_posts_controller.rb
@@ -2,3 +2,15 @@ get '/favorite' do
   @favorites = FavoritePost.where(user_id: current_user.id)
   erb :'favorite_posts/index'
 end
+
+post '/favorite' do
+  p params
+  favorite = FavoritePost.find_by(post_id: params[:post_id])
+  if favorite == nil
+    FavoritePost.create(post_id: params[:post_id], user_id: current_user.id)
+    redirect back
+  else
+    favorite.destroy
+    redirect back
+  end
+end
