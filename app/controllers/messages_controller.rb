012c84620c3c3c32ba15fc4d5d7d344eebca2a8e
Dmitri Stefanisin <email.ds@protonmail.com>
2018-01-15 10:24:44 -0600

    more work on message routes, not complited

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 get '/messages' do
+  @conversations = Message.where(author_id: currtent_user.id) # ?????
   erb :'messages/index'
 end
 
@@ -10,9 +11,17 @@ post '/messages' do
   authenticate!
   @message = Message.new(content: params[:content], author_id: currtent_user.id, receiver_id: params[:id])
   if @message.save
-    redirect back
+    redirect "/messages/#{params[:id]}"
   else
     @errors = @message.errors.full_messages
     redirect back
   end
+end
+
+get '/messages/:id' do
+  authenticate!
+  @messages = Message.where(receiver_id: [params[:id],currtent_user.id], author_id: [currtent_user.id, params[:id]]) #order('created_at')
+  # @messages = Message.where("receiver_id = ? AND author_id = ?",params[:id], currtent_user.id)
+  # @messages << Message.where("receiver_id = ? AND author_id = ?", currtent_user.id,params[:id])
+  erb :'messages/show'
 end
