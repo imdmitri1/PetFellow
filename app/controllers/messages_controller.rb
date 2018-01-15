2155174fc9c2835de51df9afdb2d7a6c85562ed2
Dmitri Stefanisin <email.ds@protonmail.com>
2018-01-15 12:07:37 -0600

    add form for new massages in show, messaging works, but need to fix bugs: ordering and prevent from sending messages youself

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,17 @@
 get '/messages' do
-  @conversations = Message.where(author_id: current_user.id) 
+  @conversations = Message.where(author_id: current_user.id)
   erb :'messages/index'
 end
 
-get '/messages/new' do
-  erb :'messages/new'
-end
+# get '/messages/new' do
+#   erb :'messages/new'
+# end
 
 post '/messages' do
   authenticate!
-  @message = Message.new(content: params[:content], author_id: current_user.id, receiver_id: params[:id])
+  @message = Message.new(content: params[:content], author_id: current_user.id, receiver_id: params[:receiver])
   if @message.save
-    redirect "/messages/#{params[:id]}"
+    redirect "/messages/#{params[:receiver]}"
   else
     @errors = @message.errors.full_messages
     redirect back
@@ -20,6 +20,7 @@ end
 
 get '/messages/:id' do
   authenticate!
+  @receiver = params[:id]
   @messages = Message.where(receiver_id: [params[:id],current_user.id], author_id: [current_user.id, params[:id]]) #order('created_at')
   # @messages = Message.where("receiver_id = ? AND author_id = ?", params[:id], current_user.id)
   # @messages << Message.where("receiver_id = ? AND author_id = ?", current_user.id, params[:id])
