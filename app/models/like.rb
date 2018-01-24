b7f678a29c4c60a480305962d3f8dfda396332d5
Dmitri Stefanisin <email.ds@protonmail.com>
2018-01-23 21:41:39 -0600

    fix little things: jumbotron in mobile view, change background, add 'likes' word

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,8 @@ class Like < ActiveRecord::Base
   validates_presence_of :likable_id, :likable_type, :author_id
 
   def self.total_count
-    self.all.map(&:count).reduce(:+)
+    total = self.all.map(&:count).reduce(:+).to_i
+    return "1 like" if total == 1
+    return "#{total} likes" if total > 1
   end
 end
